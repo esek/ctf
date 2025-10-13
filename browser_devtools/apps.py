a714688db30899c96cbf8c385019002931ee0a1e
Joelle Linder <joelsc.linder@gmail.com>
2025-10-13 20:59:48 +0200

    Starting work on new websockets based task

--- browser_devtools/apps.py
+++ browser_devtools/apps.py
@@ -5,4 +5,5 @@ class BrowserDevtoolsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "browser_devtools"
     has_tasks = True
+    use_docker = True
     display_name = "Browser Devtools"
--- browser_devtools/views.py
+++ browser_devtools/views.py
@@ -7,3 +7,7 @@ from ctef_core.decorators import define_task
 @define_task(name="Watch your head!", clues="clues/watch_your_head.md")
 def watch_your_head(request: HttpRequest, context):
     return render(request, "browser_devtools/watch_your_head.html", context)
+
+@define_task(name="Courier intercept")
+def courier_intercept(request: HttpRequest, context):
+    return render(request, "browser_devtools/courier_intercept", context)
